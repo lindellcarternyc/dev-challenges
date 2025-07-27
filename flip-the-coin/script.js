b42256abbd6054f465e47c894a4dc058a7694011
Lindell Carter <lindellcarter@Lindells-MacBook-Air.local>
2025-07-27 16:09:21 -0400

    update links

--- flip-the-coin/script.js
+++ flip-the-coin/script.js
@@ -8,7 +8,7 @@ const flipCoin = () => {
 
   setTimeout(() => {
     const isHeads = Math.random() < 0.5;
-    $coin.src = isHeads ? "resources/heads.svg" : "resources/tails.svg";
+    $coin.src = isHeads ? "./resources/heads.svg" : "./resources/tails.svg";
     $coin.alt = isHeads ? "Heads" : "Tails";
     $result.innerText = isHeads ? "Heads" : "Tails";
     $coin.classList.remove("flipping");
