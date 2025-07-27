896b284234b7b48ba51f9f042fe9c663eacb6096
Lindell Carter <lindellcarter@Lindells-MacBook-Air.local>
2025-07-27 16:02:31 -0400

    flip the coin

---
+++ flip-the-coin/script.js
@@ -0,0 +1,16 @@
+// Dom Elements
+const $coin = document.getElementById("coin-img");
+const $button = document.getElementById("random-btn");
+const $result = document.getElementById("result");
+
+const flipCoin = () => {
+  $coin.classList.add("flipping");
+
+  setTimeout(() => {
+    const isHeads = Math.random() < 0.5;
+    $coin.src = isHeads ? "resources/heads.svg" : "resources/tails.svg";
+    $coin.alt = isHeads ? "Heads" : "Tails";
+    $result.innerText = isHeads ? "Heads" : "Tails";
+    $coin.classList.remove("flipping");
+  }, 600);
+};
